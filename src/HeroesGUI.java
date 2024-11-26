a0527bcaf4870488f273808961aaf334d1981072
martinizin <mar45pillacela@gmail.com>
2024-11-26 11:04:14 -0500

    Creacion del GUI Form

---
+++ src/HeroesGUI.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HeroesGUI {
+    private JPanel pGeneral;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JButton REGISTRARHEROEButton;
+    private JButton MODIFICARHEROEButton;
+    private JButton CALCULARDETALLESButton;
+    private JTextArea textArea1;
+
+
+    public HeroesGUI() {
+        REGISTRARHEROEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        MODIFICARHEROEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        CALCULARDETALLESButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
